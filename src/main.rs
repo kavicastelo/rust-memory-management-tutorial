7275d45a9225006bde512729c9afe35605e2c798
kavi castelo <kavindu123450@gmail.com>
2024-08-05 19:11:06 +0530

    basics of memory management
    stack, heap, and stack_overflow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    stack_allocation();
+    heap_allocation();
+    // stack_overflow(); uncomment to see stack overflow error
+}
+
+fn stack_allocation() {
+    let x = 5;
+    let y = x;
+    println!("Stack allocation: x = {}, y = {}", x, y);
+}
+
+fn heap_allocation() {
+    let x = Box::new(5);
+
+    println!("Heap allocation: x = {}", x);
+}
+
+fn stack_overflow() {
+    let large_array = [0u8; 10_000_000];
+    println!("Large array on stack: {:?}", large_array);
 }
